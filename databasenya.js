2c3c04d530400975da31f4207122df4a3e0a9311
SRTEAMBROOOOOOREAL <srt88253@gmail.com>
2024-12-09 18:54:32 +0700

    Create databasenya.js

---
+++ databasenya.js
@@ -0,0 +1,23 @@
+{
+  "keys": [
+    {
+      "key": "Ryzzzx1",
+      "key": "Ryzzzx2",
+      "key": "Ryzzzx3",
+      "key": "Ryzzzx4",
+      "key": "Ryzzzx5",
+      "key": "Ryzzzx6",
+      "key": "Ryzzzx7",
+      "key": "Ryzzzx8",
+      "key": "Ryzzzx9",
+      "key": "BELI LAH MEK🗿",
+      "numbers": [
+        "6285770584801",
+        "62857277639357",
+        "62",
+        "62",
+        "62"
+      ]
+    }
+  ]
+}
